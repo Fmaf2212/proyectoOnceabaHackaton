5e7e284e192f1b36b0a3d40b3fac0f0af476a9b2
moises <fmaf_3011@outlook.com>
2021-02-20 13:05:47 -0500

    add responsive and js

---
+++ js/script.js
@@ -0,0 +1,75 @@
+// VARIABLES
+const listaCursos = document.querySelector('.carreras__content');
+const cursos = document.getElementById('cursos');
+const cursos2 = document.getElementById('cursos2');
+const cursos3 = document.getElementById('cursos3');
+// console.log(listaCursos);
+// LISTENERS
+initAPP();
+document.addEventListener('load', initAPP);
+function initAPP() {
+    // console.log('hola mundo')
+    listaCursos.addEventListener('click', obtenerCurso)
+}
+// FUNCIONES
+function obtenerCurso(e) {
+    // console.log(e.target)
+    if (e.target.classList.contains('agregarCarrito')) {
+        const curso = e.target.parentElement;
+        const curso2 = e.target.parentElement;
+        const curso3 = e.target.parentElement;
+        extraerDatos(curso,curso2,curso3);
+    }
+}
+
+
+function extraerDatos(curso,curso2,curso3) {
+    // console.log(curso)
+    const cursoElegido = {
+        span: curso.querySelector('span').textContent
+    }
+    const cursoElegido2 = {
+        nombre: curso2.querySelector('h3').textContent,
+    }
+    const cursoElegido3 = {
+        precio: curso3.querySelector('strong').textContent,
+    }
+    // console.log(cursoElegido)
+    pintarCurso(cursoElegido,cursoElegido2,cursoElegido3);
+}
+
+function pintarCurso(curso,curso2,curso3) {
+    let container2 = document.createElement('div');
+    let container3 = document.createElement('div');
+    let img = document.createElement('img');
+    let nombre = document.createElement('p');
+    let precio = document.createElement('p');
+    let span;
+    img.src='img/imgProduct2.jpg';
+    span=curso.span;
+
+    nombre = curso2.nombre;
+    precio = curso3.precio;
+    container2.innerHTML = `
+        <div>
+           ${nombre}
+        </div>
+    `
+    container3.innerHTML = `
+        <div>
+            ${precio}
+        </div>
+    `
+
+    cursos2.appendChild(container2);
+    cursos3.appendChild(container3);
+    if(span==2){
+        img.src='img/imgProduct2.jpg';
+        cursos.appendChild(img); 
+    }
+    else if(span==1){
+        img.src='img/imgProduct1.png';
+        cursos.appendChild(img); 
+    }
+    
+}
